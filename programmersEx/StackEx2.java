d0ec40d083e1a1683a57dc411a35027e78401ef1
sumanChoi <voltovo@naver.com>
2022-07-30 13:17:04 +0900

    FIX:주식 가격 다른 풀이 분석

--- programmersEx/StackEx2.java
+++ programmersEx/StackEx2.java
@@ -16,16 +16,29 @@ public class StackEx2 {
         int[] answer = {};
         answer = new int[prices.length];
 
+        // 다른 사람 풀이 분석
         for (int i = 0; i < prices.length; i++) {
             for (int j = i + 1; j < prices.length; j++) {
-                if(prices[i] > prices[j]){
-                    answer[i] = j - i;
+                answer[i]++;    // 주식을 산 시점에서 부터 1씩 증감
+                                // 1. 중간에 가격이 떨어진 시점
+                                // 2. 배열의 끝까지 가격이 떨어 지지 않은 시점
+                                // 모두 카운티이 가능 .... 대단쓰
+                if(prices[i] > prices[j])
                     break;
-                }else if(j == prices.length - 1){
-                    answer[i] = j - i;
-                }
             }
         }
+
+        // 나의 풀이
+        // for (int i = 0; i < prices.length; i++) {
+        //     for (int j = i + 1; j < prices.length; j++) {
+        //         if(prices[i] > prices[j]){
+        //             answer[i] = j - i;
+        //             break;
+        //         }else if(j == prices.length - 1){
+        //             answer[i] = j - i;
+        //         }
+        //     }
+        // }
         return answer;
     }
 }
