c18727e8e9b8d0f3e8dbf35ddae66d3e2d15abd9
sumanChoi <voltovo@naver.com>
2022-10-22 20:28:28 +0900

    FEAT:배열 회전시키기 다른 풀이 참고

--- programmersEx/ArrayRotation.java
+++ programmersEx/ArrayRotation.java
@@ -1,31 +1,36 @@
 package programmersEx;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class ArrayRotation {
     public static void main(String[] args) {
         int[] numbers = { 1, 2, 3, 4, 5 };
 
-        System.out.println("배열 회전 : " + Arrays.toString(solution(numbers, "left")));
+        System.out.println("배열 회전 : " + Arrays.toString(solution(numbers, "right")));
 
     }
 
     private static int[] solution(int[] numbers, String direction) {
         int[] answer = {};
-        List<Integer> list = Arrays.stream(numbers).boxed().collect(Collectors.toList());
-        if("right".equals(direction)){
-            int temp = list.get(list.size() - 1);
-            list.remove(list.size() - 1);
-            list.add(0, temp);
-        }else{
-            int temp = list.get(0);
-            list.remove(0);
-            list.add(list.size(), temp);
+
+        answer = new int[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++){
+            if("left".equals(direction)){
+                if(i == 0){
+                    answer[numbers.length - 1] = numbers[i];
+                }else{
+                    answer[i - 1] = numbers[i];
+                }
+            }else{
+                if(i == numbers.length - 1){
+                    answer[0] = numbers[i];
+                }else{
+                    answer[i + 1] = numbers[i];
+                }
+            }
         }
 
-        answer = list.stream().mapToInt(i -> i).toArray();
         return answer;
     }
 }
