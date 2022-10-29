910aaa75d6301c08548414003c1c20f729631989
sumanChoi <voltovo@naver.com>
2022-10-29 15:15:39 +0900

    FEAT: 최댓값 구하기 다른 사람 풀이 참고 후 수정

--- programmersEx/MaxValue2.java
+++ programmersEx/MaxValue2.java
@@ -1,11 +1,11 @@
 package programmersEx;
 
-import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
 
 public class MaxValue2 {
+
     public static void main(String[] args) {
-        int[] numbers = {1,-1};
+        int[] numbers = {1, 2, -3, 4, -5};
 
         System.out.println("곱하기 최대 값 구하기 : " + solution(numbers));
 
@@ -13,18 +13,18 @@ public class MaxValue2 {
 
     private static int solution(int[] numbers) {
         int answer = 0;
-        // 곱한 값을 보관할 list 생성
-        ArrayList<Integer> multiList = new ArrayList<>();
-        for(int i = 0; i < numbers.length; i++){
-            for (int j = i + 1; j < numbers.length; j++){
-                // 곱한 값을 list에 보관
-                multiList.add(numbers[i] * numbers[j]);
-            }
-        }
-        // list를 내림차순으로 정렬 (높은 수가 제일 앞에 옴)
-        multiList.sort(Comparator.reverseOrder());
-        // 첫번쨰 숫자가 제일 큼료
-        answer = multiList.get(0);
+
+        // 배열을 오른차순으로 정렬 (작은 수 --> 큰 수)
+        Arrays.sort(numbers);
+
+        int length = numbers.length;
+        // 큰 수가 나오는 경우 두 가지
+        // 음수 * 음수 / 양수 * 양수
+        // 음수가 있는 앞에서 부터 곱하고,  양수 중에 큰 수가 있는 뒤에서 부터 곱해서
+        // 두 값을 비교 후 결과 값 정하기
+        answer = numbers[length - 1] * numbers[length - 2] < numbers[0] * numbers[1] ? numbers[0]
+            * numbers[1] : numbers[length - 1] * numbers[length - 2];
+
         return answer;
     }
 }
