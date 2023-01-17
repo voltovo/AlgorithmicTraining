e691ba161430e3aca2462b5d09540d9870a94b6a
sumanChoi <voltovo@naver.com>
2023-01-17 23:31:35 +0900

    feat:팩토리얼 다른 사람 풀이 분석

--- programmersEx/Factorial.java
+++ programmersEx/Factorial.java
@@ -4,23 +4,25 @@ public class Factorial {
 
     public static void main(String[] args) {
 
-        System.out.println("펙토리얼 제일 큰 정수 = " + solution(2));
+        System.out.println("펙토리얼 제일 큰 정수 = " + solution(7));
     }
 
     private static int solution(int n){
+        // 횟수 체크
         int answer = 1;
-        for (int i = 1; i <= n + 1; i++){
-            for (int j = i; j > 0; j--){
-                answer *= j;
-            }
-            System.out.println("answer = " + answer);
-            if(n < answer){
-                answer = i -1;
-                break;
-            }
-            answer = 1;
+        // 자연수 펙토리얼 수행
+        int factorial = 1;
+
+        while (n >= factorial){
+            answer ++;
+            factorial *= answer;
         }
-        return answer;
+        // 조건문으로 실행된 한번 빼기
+        // 횟수 확인을 위해서 for문 한번
+        // 팩토리얼 수행을 위해서 for문 한번
+        // 총 두번의 for문이 필요 할 거라고 생각했지만
+        // answer로 횟수 및 정답을 유출하면서 반목문 한번 수행
+        return answer - 1;
     }
 
 }
