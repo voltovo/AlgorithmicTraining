69a57c614fa6187cfa50fbbd0fbead571aeddd74
sumanChoi <voltovo@naver.com>
2023-02-26 21:45:07 +0900

    feat: code refactoring

--- programmersEx/getHighestNumber.java
+++ programmersEx/getHighestNumber.java
@@ -2,13 +2,12 @@ package programmersEx;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
 
 public class getHighestNumber {
     public static void main(String[] args) {
-        int[] array = {1};
+        int[] array = {1,1,2,2};
         System.out.println("제일 많은 값은 = " + solution(array));
     }
 
@@ -17,11 +16,10 @@ public class getHighestNumber {
         HashMap<Integer, Integer> numberMap = new HashMap<Integer, Integer>();
 
         // 배열의 수 카운팅
-        for (int i = 0; i < array.length; i++){
-            int number = array[i];
-            if(numberMap.containsKey(number)){
-                numberMap.put(number, (int)numberMap.get(number) + 1);
-            }else{
+        for (int number : array) {
+            if (numberMap.containsKey(number)) {
+                numberMap.put(number, numberMap.get(number) + 1);
+            } else {
                 numberMap.put(number, 1);
             }
         }
@@ -30,18 +28,14 @@ public class getHighestNumber {
         int highestValue = 0;
         // 카운팅 된 맵에서 제일 큰 수 구하기
         List<Integer> highestKeyList = new ArrayList<>();
-        Iterator<Entry<Integer, Integer>> it = numberMap.entrySet().iterator();
-        while(it.hasNext()){
-            Entry<Integer, Integer> entrySet = (Entry<Integer, Integer>) it.next();
-            if(highestValue < entrySet.getValue()){
+        for (Entry<Integer, Integer> entrySet : numberMap.entrySet()) {
+            if (highestValue < entrySet.getValue()) {
                 highestValue = entrySet.getValue();
             }
         }
 
         // 제일 큰 수가 몇개 있는지 구하기
-        Iterator<Entry<Integer, Integer>> it2 = numberMap.entrySet().iterator();
-        while(it2.hasNext()){
-            Entry<Integer, Integer> entrySet = (Entry<Integer, Integer>) it2.next();
+        for(Entry<Integer, Integer> entrySet : numberMap.entrySet()){
             if(highestValue == entrySet.getValue()){
                 highestKeyList.add(entrySet.getKey());
             }
