a0078961af6319064c0c9b3bcb7b504b1c08cd1e
sumanChoi <voltovo@naver.com>
2023-04-22 17:27:09 +0900

    feat:a와 b 출력하기, 다른 사람 풀이 분석 완료

--- programmersEx/PrintOutOne.java
+++ programmersEx/PrintOutOne.java
@@ -3,11 +3,14 @@ package programmersEx;
 import java.util.Scanner;
 
 public class PrintOutOne {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String numberStr = sc.nextLine();
-        String[] numberArray = numberStr.split(" ");
-        System.out.println("a = " + numberArray[0]);
-        System.out.println("b = " + numberArray[1]);
+        // 입력 받은 값 중에 공백 제거 하고 int만 가지고 옴
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
     }
 }
