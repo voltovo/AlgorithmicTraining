6469c8eb5b2634663fa8dad5c4f0a7694ff0cc91
sumanChoi <voltovo@naver.com>
2023-08-08 18:52:19 +0900

    feat:stack을 활용한 문제 풀이 추가

--- programmersEx/ControlZ.java
+++ programmersEx/ControlZ.java
@@ -1,23 +1,26 @@
 package programmersEx;
 
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class ControlZ {
     public static int solution(String s) {
         int answer = 0;
-        int prev = 0;
 
-        // 데이터가 많은 경우 StringTokenizer가 더 빠름
-        // String.split()은 문자열을 다시 배열로 반환하기 때문에 느림
+        Stack<Integer> stack = new Stack<>();
         StringTokenizer tokenizer = new StringTokenizer(s, " ");
-        while (tokenizer.hasMoreTokens()){
-            String str = tokenizer.nextToken();
-            if("Z".equals(str)){
-                answer -= prev;
+
+        while(tokenizer.hasMoreTokens()){
+            String token = tokenizer.nextToken();
+            if("Z".equals(token)) {
+                stack.pop();
             }else{
-                prev = Integer.parseInt(str);
-                answer += prev;
+                stack.push(Integer.parseInt(token));
             }
+        }
+
+        while(!stack.isEmpty()){
+            answer += stack.pop();
         }
         return answer;
     }
