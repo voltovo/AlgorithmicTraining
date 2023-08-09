3bc9f99243b346c304c08e6e18310991cd383de4
sumanChoi <voltovo@naver.com>
2023-08-09 23:57:15 +0900

    feat:영어가 싫어요 다른 풀이 분석

--- programmersEx/HateEnglish.java
+++ programmersEx/HateEnglish.java
@@ -1,2 +1,16 @@
-package programmersEx;public class HateEnglish {
+package programmersEx;
+
+public class HateEnglish {
+    public static long solution(String numbers) {
+        long answer = 0;
+        String[] englishStrArray = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        // 영어 스펠링 중복이 없으므로, 해당 영어 스펠링을 숫자로 바꿔줌
+        for(int i = 0; i < englishStrArray.length; i++){
+            numbers = numbers.replaceAll(englishStrArray[i], String.valueOf(i));
+        }
+
+        answer = Long.parseLong(numbers);
+        return answer;
+    }
 }
--- programmersEx/TestCode/programmersEx/HateEnglishTest.java
+++ programmersEx/TestCode/programmersEx/HateEnglishTest.java
@@ -1,4 +1,32 @@
-import static org.junit.Assert.*;
+package programmersEx;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
 public class HateEnglishTest {
-  
+    private String input;
+    private long expected;
+
+    public HateEnglishTest(String input, long expected) {
+        this.input = input;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+            { "onetwothreefourfivesixseveneightnine", 123456789 },
+            { "onefourzerosixseven", 14067 }
+        });
+    }
+    @Test
+    public void solution() {
+        assertEquals(expected, HateEnglish.solution(input));
+    }
 }
