44ea4d16a021194972e4e228c874b9540ac4ec38
sumanChoi <voltovo@naver.com>
2023-10-09 12:02:57 +0900

    feat:다음에 올 숫자 - 다른 사람 풀이 참고

--- programmersEx/NextNumber.java
+++ programmersEx/NextNumber.java
@@ -5,22 +5,13 @@ public class NextNumber {
     public static int solution(int[] common) {
         boolean isArithmetic = true;
         int length = common.length;
-        int diff = common[1] - common[0];
+        int diff1 = common[1] - common[0];
+        int diff2 = common[2] - common[1];
 
-        for (int i = 0; i < length - 1; i++) {
-            int d = common[i + 1] - common[i];
-
-            if(d != diff){
-                isArithmetic = false;
-                diff = common[i + 1] / common[i];
-                break;
-            }
-        }
-
-        if(isArithmetic){
-            return common[length - 1] + diff;
-        }else{
-            return common[length - 1] * diff;
+        if (diff1 == diff2) {
+            return common[length - 1] + diff2;
+        } else {
+            return common[length - 1] * common[2] / common[1];
         }
     }
 }
