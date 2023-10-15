f746efeb4e92ef855b5e417e7259e75bbdd78657
sumanChoi <voltovo@naver.com>
2023-10-15 20:37:53 +0900

    feat:가우스 합을 이용해서 부족한 금액 구하기 풀이

--- programmersEx/InsufficientAmount.java
+++ programmersEx/InsufficientAmount.java
@@ -2,14 +2,9 @@ package programmersEx;
 
 public class InsufficientAmount {
     public static long solution(int price, int money, int count){
-        long sum = 0;
-        for (int i = 1; i <= count; i++) {
-            sum += price * i;
-        }
-        if(sum > money){
-            return sum - money;
-        }else{
-            return 0;
-        }
+        // 가우스 합을 이용
+        //long sum = (long) price * count * (count + 1) / 2;
+
+        return Math.max((long) price * count * (count + 1) / 2 - money, 0);
     }
 }
